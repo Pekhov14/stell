ea501f77f64dc528e3c46e6015f0d151706b6407
Pekhov14 <pehovanton21@gmail.com>
2021-10-02 03:49:05 +0300

    1.0.7

--- src/Stell.php
+++ src/Stell.php
@@ -3,9 +3,29 @@
 
 class Stell
 {
+    private $appDir;
+
+    public function __construct($dir)
+    {
+        $this->appDir = $dir;
+    }
+
     public function make()
     {
-        return 0;
+        date_default_timezone_set('UTC');
+
+        $fileName = 'update' . date('Y-m-d');
+
+        echo "created file for migration - \033[32m  $fileName \033[0m " . PHP_EOL;
+
+        $sqlDir = $this->appDir . '/sql';
+
+        if (!file_exists($sqlDir)) {
+            mkdir($sqlDir, 0777, true);
+        }
+
+        $handle = file_put_contents($sqlDir . "/$fileName.php", "<?php \n\n", LOCK_EX);
+
     }
     public function migrate()
     {
@@ -13,6 +33,19 @@ class Stell
     }
     public function help()
     {
-        echo 'help';
+        echo "\033[31m Hello, it`s stell. \033[0m Helper for opencart migrations \n";
+
+        echo PHP_EOL;
+        echo '       /‾‾‾‾‾‾‾‾/ |‾‾‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾‾|  |‾‾|         |‾‾|        ' . PHP_EOL;
+        echo '      /  ___   /  |____   ____|  |          |  |  |         |  |        ' . PHP_EOL;
+        echo '     /  /  /__/        |  |      |   |‾‾‾‾‾‾   |  |         |  |        ' . PHP_EOL;
+        echo '     \  \              |  |      |   |         |  |         |  |        ' . PHP_EOL;
+        echo '      \  \             |  |      |    ‾‾‾‾‾‾|  |  |         |  |        ' . PHP_EOL;
+        echo '       \  \            |  |      |   |‾‾‾‾‾‾   |  |         |  |        ' . PHP_EOL;
+        echo '  |‾‾|  \  \           |  |      |   |         |  |         |  |        ' . PHP_EOL;
+        echo '  |  |___|  |          |  |      |    ‾‾‾‾‾‾|  |   ‾‾‾‾‾‾‾| |   ‾‾‾‾‾‾‾|' . PHP_EOL;
+        echo '  |_________/          |__|      |__________|  |__________| |__________|' . PHP_EOL;
+
+        echo "\033[32m Stell \033[0m version \033[33m 1.0.7 \033[0m \n";
     }
 }
